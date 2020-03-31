27ff61c3146a6783f4e8cec3b0713a8432cd3843
orh92 <orh92@walla.co.il>
2020-03-31 15:52:28 +0300

    Move to Opening Screen

--- app/src/main/java/com/example/privateapplication/MainActivity.java
+++ app/src/main/java/com/example/privateapplication/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.privateapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button clickToStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clickToStart=findViewById(R.id.clickToStart);
+        clickToStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,OpeningScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/privateapplication/OpeningScreen.java
@@ -0,0 +1,14 @@
+package com.example.privateapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OpeningScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+    }
+}

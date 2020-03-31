975268096619dc7a536beb3f638f1602385f147d
orh92 <orh92@walla.co.il>
2020-04-01 02:56:39 +0300

    Design refactor

--- app/src/main/java/com/example/privateapplication/Attending.java
+++ app/src/main/java/com/example/privateapplication/Attending.java
@@ -3,6 +3,8 @@ package com.example.privateapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Attending extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class Attending extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attending);
+
+           //spinner info slots
+        String[] hours = new String[]{
+                "8:00",  "9:00", "10:00"
+        };
+        //type spinner
+        final Spinner spinnerOptions = findViewById(R.id.chooseOptions);
+        ArrayAdapter<String> optionsAdp = new ArrayAdapter<String>(Attending.this,android.R.layout.simple_spinner_item, hours);
+        optionsAdp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerOptions.setAdapter(optionsAdp);
     }
+
+
+
 }

4a959cfa75d11010a8882941420669afca02fa29
orh92 <orh92@walla.co.il>
2020-03-31 16:26:30 +0300

    Add buttons to go another activity (Forms, PrivateProfile).

--- app/src/main/java/com/example/privateapplication/OpeningScreen.java
+++ app/src/main/java/com/example/privateapplication/OpeningScreen.java
@@ -2,13 +2,35 @@ package com.example.privateapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OpeningScreen extends AppCompatActivity {
+    Button forms;
+    Button privateProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
+        forms=findViewById(R.id.forms);
+        privateProfile=findViewById(R.id.privateProfile);
+        forms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(OpeningScreen.this,Forms.class);
+                startActivity(intent);
+
+            }
+        });
+        privateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(OpeningScreen.this,PrivateProfile.class);
+                startActivity(intent);
+            }
+        });
     }
 }

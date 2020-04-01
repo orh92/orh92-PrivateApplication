6343526f83191c1f649da135a74117f57312e91a
orh92 <orh92@walla.co.il>
2020-04-01 13:38:16 +0300

    Fix design bugs

--- app/src/main/java/com/example/privateapplication/Logistica.java
+++ app/src/main/java/com/example/privateapplication/Logistica.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -32,7 +33,7 @@ Button accept;
 
         //spinner info slots
         String[] professions = new String[]{
-                "נא לבחור מהרשימה","מא\"ג", "נגביסט","קלע","צלף","חובש"
+                "נא לבחור מהרשימה","מא\"ג", "נגביסט","קלע","צלף","חובש","מטול"
         };
         //type proffession
         final Spinner profession = findViewById(R.id.chooseOptions3);
@@ -45,17 +46,29 @@ Button accept;
         String[] elements = new String[]{
                 "נא לבחור מהרשימה","משקפי מגן","מעיל ב'" ,"ברכיות", "ווסט לוחם","מנשא 90 ל'","קסדה'","מדי ב'"
         };
+
+
         //element spinner
         final Spinner elementsSp = findViewById(R.id.chooseOptions4);
         ArrayAdapter<String> elementsAdp = new ArrayAdapter<String>(Logistica.this,android.R.layout.simple_spinner_item, elements);
         elementsAdp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         elementsSp.setAdapter(elementsAdp);
+/*
+        profession.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String  st = parent.getItemAtPosition(position).toString();
+                if (!(st.equals("נא לבחור מהרשימה"))) {
+                    elementsSp.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
-
+ */
 
         //spinner info slots
         String[] size = new String[]{
-                "0 - 1","0 - 2","0 - 3","0 - 4","0 - 5","נא לבחור מהרשימה"
+               "נא לבחור מהרשימה", "0 - 1","0 - 2","0 - 3","0 - 4","0 - 5"
         };
 
         //type size
@@ -63,16 +76,30 @@ Button accept;
         ArrayAdapter<String> sizeAdp = new ArrayAdapter<String>(Logistica.this,android.R.layout.simple_spinner_item, size);
         sizeAdp.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sizeSp.setAdapter(sizeAdp);
-        if(!(roles.toString().equals("נא לבחור מהרשימה"))){profession.setVisibility(View.VISIBLE);}
+
+        /*
+        elementsSp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String  st = parent.getItemAtPosition(position).toString();
+                if (!(st.equals("נא לבחור מהרשימה"))) {
+                    sizeSp.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+         */
 
         accept=findViewById(R.id.button6);
         accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!(role.toString().equals("נא לבחור מהרשימה"))){Intent intent=new Intent(Logistica.this,FinishProccess.class);
+                if(!(profession.getSelectedItem().toString().equals("נא לבחור מהרשימה"))){
+                    Intent intent=new Intent(Logistica.this,FinishProccess.class);
                     startActivity(intent);}
                 else{
-                    Toast toast=Toast.makeText(getApplicationContext(),"לא ניתן להמשיך, כי לא נבחר תפקיד.", Toast.LENGTH_SHORT);
+                    Toast toast=Toast.makeText(Logistica.this,"לא ניתן להמשיך, כי לא נבחר תפקיד.", Toast.LENGTH_SHORT);
+                    toast.show();
                 }
 
             }
